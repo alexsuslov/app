bfe34453ff12bb4ee88464e8c2cb39d157fe6ca0
Alex Suslov <suslov@me.com>
2020-06-30 10:51:13 +0300

    Create app.go

---
+++ app.go
@@ -0,0 +1,37 @@
+type App struct {
+	logger bool
+	err error
+}
+
+func (App App) Error()string{
+	return v.Error()
+}
+
+func (App *App) IsErr(errs ...error) bool {
+	if App.err != nil {
+		return true
+	}
+	for _, err := range errs {
+		if err != nil {
+			App.err = err
+			return true
+		}
+	}
+	return false
+}
+
+func (App App) Println(v ...interface{}) {
+	if App.logger {
+		log.Println(v...)
+	}
+}
+
+func (App App) Printf(format string, v ...interface{}) {
+	if App.logger {
+		log.Printf(format, v...)
+	}
+}
+
+func (App App) Errorf(format string, v ...interface{}) {
+	log.Printf(format, v...)
+}
